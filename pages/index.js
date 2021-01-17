339d1e36f8558c9765a904f8aa5987b55ad00110
ruchiii75@gmail.com <ruchiii75@gmail.com>
2021-01-16 22:39:10 -0800

    setup grid

--- pages/index.js
+++ pages/index.js
@@ -8,35 +8,53 @@ export default function Home() {
       <Head>
         <title>paintwink</title>
         <link rel="icon" href="/favicon.ico" />
+        <style>
+          @import
+          url('https://fonts.googleapis.com/css2?family=Amatic+SC&family=Poppins:wght@300&family=Roboto:wght@100;400&display=swap');
+        </style>
       </Head>
 
-      <main className={styles.main}>
+      <Container className={styles.main}>
         <Gallery>
-          <GalleryViewer>
+          <GalleryViewer image="./images/IMG_0133.JPG">
             <ExpandoOverlay>
-              <button>CLick me</button>
+              <GalleryName>FACE PAINTINGS</GalleryName>
+              <Button>VIEW GALLERY</Button>
             </ExpandoOverlay>
           </GalleryViewer>
-          <GalleryViewer>
-            <ExpandoOverlay />
+          <GalleryViewer image="./images/IMG_0197.JPG">
+            <ExpandoOverlay>
+              <GalleryName>FACE PAINTINGS</GalleryName>
+              <Button>VIEW GALLERY</Button>
+            </ExpandoOverlay>
           </GalleryViewer>
-          <GalleryViewer>
-            <ExpandoOverlay />
+          <GalleryViewer image="./images/IMG_0204.JPG">
+            <ExpandoOverlay>
+              <GalleryName>FACE PAINTINGS</GalleryName>
+              <Button>VIEW GALLERY</Button>
+            </ExpandoOverlay>
           </GalleryViewer>
-          <GalleryViewer>
-            <ExpandoOverlay />
+          <GalleryViewer image="./images/IMG_0220.JPG">
+            <ExpandoOverlay>
+              <GalleryName>FACE PAINTINGS</GalleryName>
+              <Button>VIEW GALLERY</Button>
+            </ExpandoOverlay>
+          </GalleryViewer>
+          <GalleryViewer image="./images/IMG_0260.JPG">
+            <ExpandoOverlay>
+              <GalleryName>FACE PAINTINGS</GalleryName>
+              <Button>VIEW GALLERY</Button>
+            </ExpandoOverlay>
           </GalleryViewer>
         </Gallery>
-      </main>
+      </Container>
     </div>
   );
 }
-const GalleryViewer = styled.div`
-  padding: 250px;
-  background-size: cover;
-  background-position: center;
-  background-image: url("./images/IMG_0133.JPG");
-  position: relative;
+
+const Container = styled.div`
+  font-family: "Poppins", sans-serif;
+  letter-spacing: 4px;
 `;
 const Gallery = styled.div`
   display: grid;
@@ -44,19 +62,53 @@ const Gallery = styled.div`
   grid-gap: 5px;
   grid-template-columns: repeat(2, 600px);
 `;
+const GalleryViewer = styled.div`
+  padding: 250px;
+  background-size: cover;
+  background-position: center;
+  background-image: url(${props => props.image});
+  position: relative;
+  border-radius: 3px;
+  &:hover {
+    &::before {
+      content: "";
+      position: absolute;
+      top: 0px;
+      right: 0px;
+      bottom: 0px;
+      left: 0px;
+      background-color: rgba(0, 0, 0, 0.5);
+    }
+  }
+`;
 const ExpandoOverlay = styled.div`
   position: absolute;
   width: 100%;
   height: 100%;
-  background: #4f6c9a;
   top: 0;
   left: 0;
   opacity: 0;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   &:hover {
-    opacity: 0.5;
+    opacity: 1;
   }
 }
+`;
+const Button = styled.button`
+  font-size: 1em;
+  margin-top: 50px;
+  padding: 24px 54px;
+  border-radius: 2px;
+  color: #fff;
+  border: 2px solid #fff;
+  background: none;
+  font-family: "Poppins", sans-serif;
+  letter-spacing: 3px;
+`;
+const GalleryName = styled.div`
+  font-size: 24px;
+  color: #fff;
 `;

a81b87748f18da466d49894a194e0f7bf903fa67
ruchiii75@gmail.com <ruchiii75@gmail.com>
2021-01-16 21:24:36 -0800

    initial setup

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,62 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.scss";
+import styled from "styled-components";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>paintwink</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <Gallery>
+          <GalleryViewer>
+            <ExpandoOverlay>
+              <button>CLick me</button>
+            </ExpandoOverlay>
+          </GalleryViewer>
+          <GalleryViewer>
+            <ExpandoOverlay />
+          </GalleryViewer>
+          <GalleryViewer>
+            <ExpandoOverlay />
+          </GalleryViewer>
+          <GalleryViewer>
+            <ExpandoOverlay />
+          </GalleryViewer>
+        </Gallery>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
+const GalleryViewer = styled.div`
+  padding: 250px;
+  background-size: cover;
+  background-position: center;
+  background-image: url("./images/IMG_0133.JPG");
+  position: relative;
+`;
+const Gallery = styled.div`
+  display: grid;
+  grid-gap: 5px;
+  grid-gap: 5px;
+  grid-template-columns: repeat(2, 600px);
+`;
+const ExpandoOverlay = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: #4f6c9a;
+  top: 0;
+  left: 0;
+  opacity: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &:hover {
+    opacity: 0.5;
+  }
+}
+`;
